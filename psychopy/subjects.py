326d35b7189892a37e0ab0092f5d9906d38cfffc
aliciamchen <aliciachen@college.harvard.edu>
2021-06-24 10:32:49 -0400

    Fix sizing and also separate into trials

---
+++ psychopy/subjects.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 24 08:27:47 2021
+
+@author: aliciachen
+"""
+import random
+
+# Assign random order of problems to each subject
+
+nSubjects = 30
+nRuns = 8
+nProbsPerRun = 5
+
+nProbs = nRuns * nProbsPerRun
+
+probs = [prob for prob in range(nProbs)]
+
+subj_list = [[[] for j in range(nRuns)] for i in range(nSubjects)]
+
+for sub in range(nSubjects):
+
+    # Randomize problem list for each subject
+    random.shuffle(probs)
+
+    # Divide into nRuns sublists
+    probs_div = [probs[i:i + nProbsPerRun] for i in range(0, len(probs), nProbsPerRun)]
+
+    for run in range(nRuns):
+        subj_list[sub][run] = probs_div[run]
--- psychopy/task.py
+++ psychopy/task.py
@@ -199,223 +199,42 @@ for prob_idx, prob in enumerate(problems_raw):
                 probs[prob_idx][h][row_idx][col_idx] = value
 
 
-# Fix issue with dictionary order
+# Load problems per run from subj lists of lists for prepared problem file
+prepared_probs = {}
 
-# for idx, p in probs.items():
-#     for k, v in p.items():
-#         probs[idx][k] = {key: v[key] for key in [0, 1, 2, 3, 4, 5]}
+for subj_id, runs in enumerate(subj_list):
+    prepared_probs[subj_id] = {}
+    for run_idx, run in enumerate(runs):
+        prepared_probs[subj_id][run_idx] = {i: probs[i] for i in run}
 
-# Switch x and y
+# %%
 
-# probstest = {}
-
-# for idx, p in probs.items():
-#     probstest[idx] = {}
-#     for col_idx, v in p.items():
-#         probstest[idx][]
-#         for row_idx, val in v.items():
-#             probstest[row_idx][col_idx] =
-
-
-# %% Test first teaching problem
-
-
-
-test_prob = probs[0]
-prob_idxs = list(test_prob.keys())
-
-# Shuffle order of four images
-random.shuffle(prob_idxs)
-test_prob_shuffled = {i: test_prob[i] for i in prob_idxs}
-true_h_idx = prob_idxs.index('A')  # Extract order of index
-
-# Flatten problem to figure out colors; make color lists
-colordict = {}
-for h, val in test_prob_shuffled.items():
-    colordict[h] = []
-    for k, v in val.items():
-        for a, b in v.items():
-            colordict[h].append(b)
-
-# Convert from dict into np array to assign colors
-colorlist = []
-for k, v in colordict.items():
-    colorlist.append(v)
-
-# Assign colors
-newcolorlist = [[] for i in range(4)]
-for idx, h in enumerate(colorlist):
-    for val in h:
-        if val == 0:
-            newcolorlist[idx].append([30, 76, 124])  # Dark blue
-        else:
-            newcolorlist[idx].append([72, 160, 248])
-
-# Set locations of four hypotheses
-locs = [[-.75, .5], [-.25, .5], [.25, .5], [.75, .5]]
-
-# Draw
-win = visual.Window([800, 600], monitor="test", units="norm", color='black')
-
-# Make canvas
-canv_sq_size = .05
-canvas_loc = [0, -.5]
-
-x_low, x_high = canvas_loc[0] - 2.5*canv_sq_size, canvas_loc[0] + 2.5*canv_sq_size
-y_low, y_high = canvas_loc[1] - 2.5*canv_sq_size, canvas_loc[1] + 2.5*canv_sq_size
-
-xlocs = np.linspace(x_low, x_high, 6)
-ylocs = np.linspace(y_low, y_high, 6)
-
-# Make hypotheses
-sq_size = .05
-hs = []
-
-for i, loc in enumerate(locs):
-
-    xys = []
-    x_low, x_high = loc[0] - 2.5*sq_size, loc[0] + 2.5*sq_size
-    y_low, y_high = loc[1] - 2.5*sq_size, loc[1] + 2.5*sq_size
-
-    xs = np.linspace(x_low, x_high, 6)
-    ys = np.linspace(y_low, y_high, 6)
-
-    for y in ys:
-        for x in xs:
-            xys.append((x, y))
-
-    hs.append(visual.ElementArrayStim(win=win,
-                                      xys=xys,
-                                      colors=newcolorlist[i],
-                                      colorSpace='rgb255',
-                                      fieldShape='sqr',
-                                      nElements=36,
-                                      elementMask=None,
-                                      elementTex=None,
-                                      sizes=(.03, .03)))
-
-
-# Highlight true hypothesis
-true_h_border = visual.Rect(win,
-                            size=5*sq_size + .05,
-                            lineWidth=7,
-                            lineColor=(238, 188, 64),
-                            colorSpace='rgb255',
-                            pos=(locs[true_h_idx][0], locs[true_h_idx][1]))
-
-# Add letters on top
-letters = ['A', 'B', 'C', 'D']
-letter_locs = [(-.75, .8), (-.25, .8), (.25, .8), (.75, .8)]
-
-lets = []
-for i, l in enumerate(letter_locs):
-    lets.append(visual.TextStim(win=win,
-                                text=letters[i],
-                                pos=letter_locs[i],
-                                color='white'
-                                ))
-
-# Start instance of trial
-trial = Move()
-
-# Make blank canvas, indicating true hypothesis
-rects = {}
-for i in range(6):
-    rects[i] = {}
-    for j in range(6):
-        if test_prob['A'][i][j] == 0:
-            rects[i][j] = visual.Rect(win=win,
-                                      fillColor=(94, 93, 95),  # dark gray
-                                      colorSpace='rgb255',
-                                      pos=(xlocs[i], ylocs[j]),
-                                      units='norm',
-                                      size=canv_sq_size-.01)
-        else:
-            rects[i][j] = visual.Rect(win=win,
-                                      fillColor=(214, 214, 214),  # light gray
-                                      colorSpace='rgb255',
-                                      pos=(xlocs[i], ylocs[j]),
-                                      units='norm',
-                                      size=canv_sq_size-.01)
-
-
-# Show the example selected to the learner
-msg = visual.TextStim(win, text='Sending example to [NAME]...')
-learner_rects = {}
-for i in range(6):
-    learner_rects[i] = {}
-    for j in range(6):
-        learner_rects[i][j] = visual.Rect(win=win,
-                                          fillColor=(94, 93, 95), # dark gray
-                                          colorSpace='rgb255',
-                                          pos=(xlocs[i], ylocs[j]),
-                                          units='norm',
-                                          size=canv_sq_size-.01)
-
-
-
-# Move on and stay text boxes
-stay = visual.TextBox(window=win,
-                         text='Yes',
-                         font_size=30,
-                         #colorSpace='rgb255',
-                         opacity=1,
-                         font_color=[1, 1, 1],
-                         #background_color=[0, 0, 0],
-                         border_color=[1, 1, 1],
-                         border_stroke_width=4,
-                         textgrid_shape=[3,1],
-                         pos=(-.5, 0)
-                         )
-
-stay.setBackgroundColor([.122, .297, .486])  # Default starting key point
-
-move_on = visual.TextBox(window=win,
-                         text='No',
-                         font_size=30,
-                         opacity=1,
-                         #colorSpace='rgb255',
-                         font_color=[1, 1, 1],
-                         #background_color=[0, 0, 0],
-                         border_color=[1, 1, 1],
-                         border_stroke_width=4,
-                         textgrid_shape=[3,1],
-                         pos=(.5, 0)
-                         )
-
-buttons = [move_on, stay]
-
-# Text on top and underneath move on and stay text boxes
-cont = visual.TextStim(win=win,
-                       text="Continue?",
-                       pos=(0, .3),
-                       height=.07)
-
-yestext = visual.TextStim(win=win,
-                          text="Provide another example",
-                          pos=(-.5, -.3),
-                          height=.07)
-
-notext = visual.TextStim(win=win,
-                         text="Next problem",
-                         pos=(.5, -.3),
-                         height=.07)
-
+# Access problem based on subject and run:
+subj_id = 7
+run_idx = 5
+probs = prepared_probs[subj_id][run_idx]
 
 
 #%% actual stuff to run
 
+data = {}
+
 win = visual.Window([800, 600], monitor="test", units="norm", color='black')
 
-for k, v in probs.items():
+for prob_idx, v in probs.items():
+
+    data[prob_idx] = {}
+    data[prob_idx]['prob'] = v
+
     problemFinished = False
-    test_prob = v # probs[0]
+    test_prob = v
     prob_idxs = list(test_prob.keys())
 
     # Shuffle order of four images
     random.shuffle(prob_idxs)
     test_prob_shuffled = {i: test_prob[i] for i in prob_idxs}
     true_h_idx = prob_idxs.index('A')  # Extract order of index
+    data[prob_idx]['true_h_idx'] = true_h_idx
 
     # Flatten problem to figure out colors; make color lists
     colordict = {}
@@ -445,16 +264,6 @@ for k, v in probs.items():
     # Draw
     #win = visual.Window([800, 600], monitor="test", units="norm", color='black')
 
-    # Make canvas
-    canv_sq_size = .05
-    canvas_loc = [0, -.5]
-
-    x_low, x_high = canvas_loc[0] - 2.5*canv_sq_size, canvas_loc[0] + 2.5*canv_sq_size
-    y_low, y_high = canvas_loc[1] - 2.5*canv_sq_size, canvas_loc[1] + 2.5*canv_sq_size
-
-    xlocs = np.linspace(x_low, x_high, 6)
-    ylocs = np.linspace(y_low, y_high, 6)
-
     # Make hypotheses
     sq_size = .05
     hs = []
@@ -462,8 +271,8 @@ for k, v in probs.items():
     for i, loc in enumerate(locs):
 
         xys = []
-        x_low, x_high = loc[0] - 2.5*sq_size, loc[0] + 2.5*sq_size
-        y_low, y_high = loc[1] - 2.5*sq_size, loc[1] + 2.5*sq_size
+        x_low, x_high = loc[0] - 3*sq_size, loc[0] + 3*sq_size
+        y_low, y_high = loc[1] - 4*sq_size, loc[1] + 4*sq_size
 
         xs = np.linspace(x_low, x_high, 6)
         ys = np.linspace(y_low, y_high, 6)
@@ -480,20 +289,20 @@ for k, v in probs.items():
                                           nElements=36,
                                           elementMask=None,
                                           elementTex=None,
-                                          sizes=(.03, .03)))
+                                          sizes=(.05, .0666)))
 
 
     # Highlight true hypothesis
     true_h_border = visual.Rect(win,
-                                size=5*sq_size + .05,
-                                lineWidth=7,
+                                size=((6*sq_size + .085), (4/3)*(6*sq_size + .085)),
+                                lineWidth=30,
                                 lineColor=(238, 188, 64),
                                 colorSpace='rgb255',
                                 pos=(locs[true_h_idx][0], locs[true_h_idx][1]))
 
     # Add letters on top
     letters = ['A', 'B', 'C', 'D']
-    letter_locs = [(-.75, .8), (-.25, .8), (.25, .8), (.75, .8)]
+    letter_locs = [(-.75, .83), (-.25, .83), (.25, .83), (.75, .83)]
 
     lets = []
     for i, l in enumerate(letter_locs):
@@ -506,6 +315,17 @@ for k, v in probs.items():
     # Start instance of trial
     trial = Move()
 
+    # Make canvas
+    canv_sq_size = .08
+    canvas_loc = [0, -.45]
+
+    x_low, x_high = canvas_loc[0] - 3*canv_sq_size, canvas_loc[0] + 3*canv_sq_size
+    y_low, y_high = canvas_loc[1] - 4*canv_sq_size, canvas_loc[1] + 4*canv_sq_size
+
+    xlocs = np.linspace(x_low, x_high, 6)
+    ylocs = np.linspace(y_low, y_high, 6)
+
+
     # Make blank canvas, indicating true hypothesis
     rects = {}
     for i in range(6):
@@ -517,14 +337,14 @@ for k, v in probs.items():
                                           colorSpace='rgb255',
                                           pos=(xlocs[i], ylocs[j]),
                                           units='norm',
-                                          size=canv_sq_size-.01)
+                                          size=(canv_sq_size, (4/3)*canv_sq_size))
             else:
                 rects[i][j] = visual.Rect(win=win,
                                           fillColor=(214, 214, 214),  # light gray
                                           colorSpace='rgb255',
                                           pos=(xlocs[i], ylocs[j]),
                                           units='norm',
-                                          size=canv_sq_size-.01)
+                                          size=(canv_sq_size, (4/3)*canv_sq_size))
 
 
     # Show the example selected to the learner
@@ -538,7 +358,7 @@ for k, v in probs.items():
                                               colorSpace='rgb255',
                                               pos=(xlocs[i], ylocs[j]),
                                               units='norm',
-                                              size=canv_sq_size-.01)
+                                              size=(canv_sq_size, (4/3)*canv_sq_size))
 
 
 
@@ -577,16 +397,16 @@ for k, v in probs.items():
     cont = visual.TextStim(win=win,
                            text="Continue?",
                            pos=(0, .3),
-                           height=.07)
+                           height=.09)
 
     yestext = visual.TextStim(win=win,
                               text="Provide another example",
-                              pos=(-.5, -.3),
+                              pos=(-.5, -.2),
                               height=.07)
 
     notext = visual.TextStim(win=win,
                              text="Next problem",
-                             pos=(.5, -.3),
+                             pos=(.5, -.2),
                              height=.07)
 
 
@@ -598,10 +418,10 @@ for k, v in probs.items():
     # starting cursor color
     if test_prob['A'][0][0] == 0:
         rects[0][0].lineColor = (218, 60, 37)  # red
-        rects[0][0].lineWidth = 6
+        rects[0][0].lineWidth = 15
     else:
         rects[0][0].lineColor = (72, 160, 248)  # blue
-        rects[0][0].lineWidth = 6
+        rects[0][0].lineWidth = 15
 
     while examplesLeft > 0:
 
@@ -636,6 +456,8 @@ for k, v in probs.items():
 
             len_allKeys1 = 0
             timer1 = core.CountdownTimer(5)
+
+            # The issue with the timer is that the items arent in
             while True: # Move on and stay buttons
 
 
@@ -652,6 +474,21 @@ for k, v in probs.items():
 
                 allKeys1 = event.getKeys()
 
+                if timer1.getTime() < 0:
+                    testtrial.select()
+                        # timer1.reset()
+                    if testtrial.cursor_loc == 1:
+                        examplesLeft == 0
+                        problemFinished = True
+                        break
+
+                    if testtrial.cursor_loc == 0:
+                        event.clearEvents()
+
+                        examplesLeft -= 1  # decrease by one example
+                        timer.reset()
+                        timer1.reset()
+                        break
 
                 if len(allKeys1) > len_allKeys1:
                     thisKey = allKeys1[-1]
@@ -664,7 +501,7 @@ for k, v in probs.items():
                         testtrial.right()
                         move_on.setBackgroundColor([.122, .297, .486]) # blue
                         stay.setBackgroundColor(None) # black
-                    if thisKey == 'space' or timer1.getTime() < 0:
+                    if (thisKey == 'space') or (timer1.getTime() < 0):
                         testtrial.select()
                         # timer1.reset()
                         if testtrial.cursor_loc == 1:
@@ -677,7 +514,7 @@ for k, v in probs.items():
                     #win.flip()
 
                     # Continue providing examples
-                    if (thisKey == 'space' or timer1.getTime() < 0) and testtrial.cursor_loc == 0:
+                    if ((thisKey == 'space') or (timer1.getTime() < 0)) and (testtrial.cursor_loc == 0):
                         event.clearEvents()
 
                         examplesLeft -= 1  # decrease by one example
@@ -687,6 +524,7 @@ for k, v in probs.items():
 
 
                 len_allKeys1 = len(allKeys1)
+                event.clearEvents()
 
             core.wait(2.0)
 
@@ -787,6 +625,23 @@ for k, v in probs.items():
 
 
                             win.flip()
+
+                            if timer1.getTime() < 0:
+
+                                testtrial.select()
+                                    # timer1.reset()
+                                if testtrial.cursor_loc == 1:
+                                    examplesLeft == 0
+                                    problemFinished = True
+                                    break
+
+                                if testtrial.cursor_loc == 0:
+                                    event.clearEvents()
+
+                                    examplesLeft -= 1  # decrease by one example
+                                    timer.reset()
+                                    timer1.reset()
+                                    break
 
                             len_allkeys1 = len(allKeys1)
                             allKeys1 = event.getKeys()
@@ -936,7 +791,7 @@ for k, v in probs.items():
                     else:
                         rects[old_cursor_loc[0]][old_cursor_loc[1]].fillColor = (214, 214, 214)  # light gray
                     rects[trial.cursor_loc[0]][trial.cursor_loc[1]].lineColor = (72, 160, 248)  # blue
-                    rects[trial.cursor_loc[0]][trial.cursor_loc[1]].lineWidth = 6
+                    rects[trial.cursor_loc[0]][trial.cursor_loc[1]].lineWidth = 15
                 except (IndexError, KeyError):
                     print('index out of range')  # TODO: change to a visual stim
 
@@ -948,12 +803,12 @@ for k, v in probs.items():
                     try:
                         if test_prob['A'][i][j] == 0 and trial.cursor_loc == [i, j]:
                             rects[i][j].lineColor = (218, 60, 37)  # red
-                            rects[i][j].lineWidth = 6
+                            rects[i][j].lineWidth = 15
                         if trial.squares[i][j] == 1:
                             rects[i][j].fillColor = (72, 160, 248)
                             if trial.cursor_loc == [i, j]:
                                 rects[i][j].lineColor = (218, 60, 37)  # red
-                                rects[i][j].lineWidth = 6
+                                rects[i][j].lineWidth = 15
                     except (IndexError, KeyError):
                         print('index out of range')  # TODO: change to a visual stim
 
@@ -976,6 +831,17 @@ for k, v in probs.items():
 
             win.flip()
 
+        data[prob_idx]['canvas'] = trial.squares
         #event.clearEvents()
 
         nKeys = len(allKeys)
+
+
+endofrun = visual.TextStim(win, text="End of run", height=0.25)
+endofrun.draw()
+win.flip()
+
+core.wait(3.3)
+
+win.close()
+core.quit()
