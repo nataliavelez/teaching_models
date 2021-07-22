c4a2a6a4273e22af2265af6e0cf938e0d97b5a7c
aliciamchen <aliciachen@college.harvard.edu>
2021-07-22 18:34:14 -0400

    ahhhhhhhh

--- model/analysis.py
+++ model/analysis.py
@@ -3,6 +3,8 @@
 1. Calculate log likelihood for each participant's total + individual choices, for literal and pragmatic learner
 """
 import pandas as pd
+from seaborn.miscplot import palplot
+from seaborn.palettes import color_palette
 import examples
 import import_problems
 
@@ -88,7 +90,7 @@ def predict(true_prob_idxs):
                 prob.selected_examples(coords)
                 
                 _, _ = prob.literal()
-                _, _ = prob.pragmatic(250)
+                _, _ = prob.pragmatic(50)
                 _ = prob.outputs() 
 
                 # Fix later: 
@@ -126,7 +128,34 @@ df_expt1_final, df_expt2_final = predict(true_prob_idxs)
 # testprob.selected_examples([(1, 1), (2, 1), (3, 1), (4, 1)])
 # _, _ = testprob.literal()
 # _, _ = testprob.pragmatic(250)
+# %% to csv
 
+
+
+# %% add participant estimated posterior
+
+df_expt2_estimates = df_expt2.groupby(level=[0, 1])['posterior'].apply(list).reset_index().set_index(['worker', 'true_prob_idx'])
+df_expt2_final_with_estimates = pd.concat([df_expt2_final, df_expt2_estimates], axis=1)
+
+# %% save 
+
+df_expt1_final.to_pickle("./df_expt1.pkl")
+df_expt2_final_with_estimates.to_pickle("./df_expt2.pkl")
+
+# %% open 
+
+
+df_1 = pd.read_pickle("./df_expt1.pkl")
+df_2 = pd.read_pickle("./df_expt2.pkl")
+# df_expt1_final.to_csv("/Users/aliciachen/Dropbox/teaching_models/data/exp1_data_with_metrics.csv")
+# df_expt2_final_with_estimates.to_csv("/Users/aliciachen/Dropbox/teaching_models/data/exp2_data_with_metrics.csv")
+# %% Explode dataframe
+
+df_1_long = df_1.explode(column=['coords', 'h1Gd_lit', 'H_lit', 'KL_lit', 'dGh1_lit', 
+                        'h1Gd_prag', 'H_prag', 'KL_prag', 'dGh1_prag'])
+
+df_2_long = df_2.explode(column=['coords', 'h1Gd_lit', 'H_lit', 'KL_lit', 'dGh1_lit', 
+        'h1Gd_prag', 'H_prag', 'KL_prag', 'dGh1_prag', 'posterior'])
 # %% 
 
 def extract_confidence(data):
@@ -184,6 +213,61 @@ class Analyze(Problem):
 # TODO: function that loops through all the participants, makes plots 
 
 
-#%% 
+#%% plots
+
+import matplotlib.pyplot as plt
 
 
+
+# %% 
+
+plt.figure()
+plt.scatter(df_2_long['h1Gd_prag'], df_2_long['posterior'])
+plt.xlabel('pragmatic model p(h1|d)')
+plt.ylabel('participant estimates')
+
+plt.figure()
+plt.scatter(df_2_long['h1Gd_lit'], df_2_long['posterior'])
+plt.xlabel('literal model p(h1|d)')
+plt.ylabel('participant estimates')
+
+plt.figure()
+plt.scatter([i for i in range(2587)], df_1_long['h1Gd_prag'])
+# %%
+
+plt.figure()
+plt.scatter(df_1_long['h1Gd_prag'], df_1_long['h1Gd_lit'])
+# %%
+
+example_part = df_2_long.xs('A10249252O9I20MRSOBVF') #.set_index('true_prob_idx')
+
+#%%
+
+plt.figure()
+plt.plot(example_part['h1Gd'])
+#%%
+
+example_part.groupby('true_prob_idx')['h1Gd_lit'].apply(list)
+# %%
+example_part.groupby('true_prob_idx').boxplot()
+# %%
+import seaborn as sns
+#sns.color_palette("Spectral", as_cmap=True)
+sns.set_palette("pastel")
+sns.set()
+#s#ns.set_theme(style="ticks")
+#%%
+
+
+
+# df = example_part.drop(labels=['coords'], axis=1).reset_index()
+df = example_part[['h1Gd_lit', 'h1Gd_prag', 'posterior']].reset_index()
+df = df.iloc[20*4:24*4, :] #df.head(16)
+df['ex'] = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
+
+s = df.select_dtypes(include='object').columns
+df[s] = df[s].astype("float")
+#%%
+sns.pairplot(df, hue='true_prob_idx', palette='Spectral')
+
+# %%
